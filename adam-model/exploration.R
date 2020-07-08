57c2dc6a7e9dafddf4e98979bbc457633c48031a
Ivan Svetunkov <ivan@svetunkov.ru>
2020-07-08 15:28:14 +0100

    Adam model try-out.

---
+++ adam-model/exploration.R
@@ -0,0 +1,41 @@
+devtools::install_github("config-i1/greybox", upgrade="never", dependencies=FALSE)
+devtools::install_github("config-i1/smooth", upgrade="never", dependencies=FALSE)
+devtools::install_github("config-i1/mes", upgrade="never", dependencies=FALSE)
+
+library(greybox)
+library(smooth)
+library(mes)
+library(zoo)
+library(statmod)
+
+## Load and Prep Data ####
+# load("../data/hw_hourly.rds")
+h2 <- fread("../data/h2_hourly.csv")
+h2[,targetTime:=as.POSIXct(arrival_1h,tz="UTC",format="%Y-%m-%dT%H:%M:%SZ")]
+h2[,targetTime_UK:=targetTime]; attributes(h2$targetTime_UK)$tzone <- "Europe/London"
+
+add_calendar_variables(h2,datetimecol = "targetTime_UK")
+
+# Load Holidays
+hols <- as.data.table(read_xlsx("../data/holiday_rugby.xlsx"))
+hols[,Date:=as.Date(Date)]
+h2[,Date:=as.Date(targetTime_UK)]
+h2 <- merge(h2,hols,by="Date",all.x=T)
+rm(hols); h2[,Date:=NULL]
+h2[,school_holiday:=as.factor(school_holiday)]
+h2[,holiday_festive_day:=as.factor(holiday_festive_day)]
+h2[,is_rug_in_Cardiff:=is_rug_in_Cardiff==1]
+h2[,is_rug_out_Cardiff:=is_rug_out_Cardiff==1]
+
+plot.ts(h2$n_attendance)
+
+# Create a zoo object
+x <- zoo(h2$n_attendance, order.by=h2$targetTime)
+# Fit the occurrence model
+oesModel <- oes(x, "MNN", h=24*7, holdout=TRUE, occurrence="direct")
+# Fit the demand sizes model
+adamModel <- adam(x, "MNM", lags=c(24,24*7), h=24*7, holdout=TRUE, initial="b", occurrence="direct")
+# Do visual diagnostics
+par(mfcol=c(3,4))
+plot(adamModel,c(1:11))
+
