00407bfe30583888ef909ed4a0fe2eb278a89273
jethrobrowell <jethrobrowell@gmail.com>
2020-11-24 17:12:26 +0000

    Updated models and results

--- gam-mgcv/study_v1.R
+++ gam-mgcv/study_v1.R
@@ -34,6 +34,14 @@ h2[,targetTime_UK:=targetTime]; attributes(h2$targetTime_UK)$tzone <- "Europe/Lo
 # plot(h2[month(targetTime_UK)%in%c(11:12,1:3),mean(n_attendance),by=hour(targetTime_UK)])
 # points(h2[month(targetTime_UK)%in%c(4:10),mean(n_attendance),by=hour(targetTime_UK)],pch=2)
 
+
+## Add smooths/level tracking
+setkey(h2,targetTime)
+h2[,unique(diff(targetTime))]
+h2[,n_att_rollmean:=frollmean(n_attendance,n=12*7*24,align = "right")]
+h2[,n_att_rollmean_lag48:=shift(n_att_rollmean,n=48,type="lag")]
+# h2[,.(plot(targetTime,n_attendance,pch="."),lines(targetTime,n_att_rollmean,col="red",lwd=3))]
+
 add_calendar_variables(h2,datetimecol = "targetTime_UK")
 
 # Load Holidays
@@ -76,10 +84,18 @@ h2[issueTime<"2018-03-01",kfold:=paste0("fold",rep(rep(1:2,each=24*7),length.out
 ## Features
 setkey(h2,issueTime,targetTime_UK)
 # Weekday groups
+# Monday, Saturday and Sunday are quite distinct...
 h2[dow%in%c("Mon","Tue","Wed","Thu","Fri"),dow2:="MonFri"]
 h2[!dow%in%c("Mon","Tue","Wed","Thu","Fri"),dow2:="SatSun"]
 h2[,dow2:=as.factor(dow2)]
 
+
+
+## Quick plot ####
+require(ggplot2)
+plot_data <- h2[,.(n_attendance=mean(n_attendance)),by=c("dow","clock_hour")]
+ggplot(data=plot_data,aes(x=clock_hour,y=n_attendance,color=dow))+
+  geom_line()
 
 ## Fit Poisson-GAM and visualise model ####
 
@@ -88,7 +104,8 @@ for(fold in unique(h2$kfold)){
   gam1 <- bam(n_attendance ~
                 # s(clock_hour,k=24,by=dow2) + t + I(t^2) +
                 # ti(doy,clock_hour,k=c(6,6)),
-                te(doy,clock_hour,k=c(6,24),by=dow2) + t,
+                # te(doy,clock_hour,k=c(6,24),by=dow2) + t,
+                dow + s(clock_hour,k=20,by=dow) + s(doy,k=6,by=t),
               data=h2[kfold!=fold & kfold!="Test",],family = poisson())
   
   h2[kfold==fold,lambda:=predict(gam1,newdata =h2[kfold==fold,],type="response")]
@@ -164,72 +181,73 @@ pinball(h2_mqr[,-c(1:2)],h2$n_attendance,kfolds = h2$kfold,ylim=c(0.3,2))
 
 ## GAMLSS ####
 
-require(gamlss.add) # for ga()
-# check out http://opisthokonta.net/?p=1157
-# Double Poisson DPO
-# Negative Binomial NBI
+# require(gamlss.add) # for ga()
+# # check out http://opisthokonta.net/?p=1157
+# # Double Poisson DPO
+# # Negative Binomial NBI
+# # 
 # 
-
-h2_gamlss <- Para_gamlss(data = h2,
-                         formula = n_attendance ~ 
-                           pb(clock_hour) + pb(doy,df=6) + t,
-                         # pvc(clock_hour,by=dow2,df=24) + pb(doy,by=clock_hour,df=6), # super slow...
-                         sigma.formula = ~pb(clock_hour),
-                         family =  NBI,#DPO, #NO,  #
-                         method=mixed(20,10))
-
-h2_gamlss_mqr <- PPD_2_MultiQR(data=h2,
-                               models = h2_gamlss,
-                               params = F)
-
-issue <- unique(h2$issueTime)[9]
-plot(h2_gamlss_mqr[h2[,which(issueTime==issue)],],
-     xlab="Lead-time [hours]",ylab="Attendance",main=paste0("Origin: ",issue," (",format(issue,"%A"),")"),
-     ylim=c(0,40),Legend = "topleft")
-
-
-reliability(h2_gamlss_mqr,h2$n_attendance)
-reliability(h2_gamlss_mqr,h2$n_attendance,subsets = h2$clock_hour)
-
-pinball(h2_gamlss_mqr,h2$n_attendance,kfolds = h2$kfold,ylim=c(0.3,2))
-
-JB_results[["NBI-gamlss"]] <- cbind(h2[,.(issueTime,targetTime_UK)],h2_gamlss_mqr)
+# h2_gamlss <- Para_gamlss(data = h2,
+#                          formula = n_attendance ~ 
+#                            pb(clock_hour) + pb(doy,df=6) + t,
+#                          # pvc(clock_hour,by=dow2,df=24) + pb(doy,by=clock_hour,df=6), # super slow...
+#                          sigma.formula = ~pb(clock_hour),
+#                          family =  NBI,#DPO, #NO,  #
+#                          method=mixed(20,10))
+# 
+# h2_gamlss_mqr <- PPD_2_MultiQR(data=h2,
+#                                models = h2_gamlss,
+#                                params = F)
+# 
+# issue <- unique(h2$issueTime)[9]
+# plot(h2_gamlss_mqr[h2[,which(issueTime==issue)],],
+#      xlab="Lead-time [hours]",ylab="Attendance",main=paste0("Origin: ",issue," (",format(issue,"%A"),")"),
+#      ylim=c(0,40),Legend = "topleft")
+# 
+# 
+# reliability(h2_gamlss_mqr,h2$n_attendance)
+# reliability(h2_gamlss_mqr,h2$n_attendance,subsets = h2$clock_hour)
+# 
+# pinball(h2_gamlss_mqr,h2$n_attendance,kfolds = h2$kfold,ylim=c(0.3,2))
+# 
+# JB_results[["NBI-gamlss"]] <- cbind(h2[,.(issueTime,targetTime_UK)],h2_gamlss_mqr)
 
 
 
 ## All Eval ####
 
 
-PB <- as.data.table(pinball(h2_mqr[,-c(1:2)],h2$n_attendance,kfolds = h2$kfold,ylim=c(0.3,2)))
-Rel <- as.data.table(reliability(h2_mqr[,-c(1:2)],h2$n_attendance))
-PB[,Method:="Poisson"]
-Rel[,Method:="Poisson"]
+# PB <- as.data.table(pinball(h2_mqr[,-c(1:2)],h2$n_attendance,kfolds = h2$kfold,ylim=c(0.3,2)))
+# Rel <- as.data.table(reliability(h2_mqr[,-c(1:2)],h2$n_attendance))
+# PB[,Method:="Poisson"]
+# Rel[,Method:="Poisson"]
+# 
+# temp <- as.data.table(reliability(h2_gamlss_mqr,h2$n_attendance))
+# temp[,Method:="Negative Binomial"]
+# Rel <- rbind(Rel,temp); rm(temp)
+# temp <- as.data.table(pinball(h2_gamlss_mqr,h2$n_attendance,kfolds = h2$kfold,ylim=c(0.3,2)))
+# temp[,Method:="Negative Binomial"]
+# PB <- rbind(PB,temp); rm(temp)
 
-temp <- as.data.table(reliability(h2_gamlss_mqr,h2$n_attendance))
-temp[,Method:="Negative Binomial"]
-Rel <- rbind(Rel,temp); rm(temp)
-temp <- as.data.table(pinball(h2_gamlss_mqr,h2$n_attendance,kfolds = h2$kfold,ylim=c(0.3,2)))
-temp[,Method:="Negative Binomial"]
-PB <- rbind(PB,temp); rm(temp)
 
 
-
-ggplot(data = PB[kfold=="All_cv",],aes(y=`Loss`,x=Quantile,group=Method)) +
-  geom_line(aes(linetype=Method, color=Method)) + 
-  theme_bw() + theme(legend.position="bottom")
-
-ggplot(data = Rel[kfold=="All",],aes(y=Empirical,x=Nominal,group=Method)) +
-  geom_line(aes(linetype=Method, color=Method)) + 
-  theme_bw() + theme(legend.position="bottom")
+# ggplot(data = PB[kfold=="All_cv",],aes(y=`Loss`,x=Quantile,group=Method)) +
+#   geom_line(aes(linetype=Method, color=Method)) + 
+#   theme_bw() + theme(legend.position="bottom")
+# 
+# ggplot(data = Rel[kfold=="All",],aes(y=Empirical,x=Nominal,group=Method)) +
+#   geom_line(aes(linetype=Method, color=Method)) + 
+#   theme_bw() + theme(legend.position="bottom")
 
 
 
 ## Now GBM... ####
 
 # Maybe need to de-trend before GBM and replace after?
+# Or lagged values/long term smooth?
 
 h2_gbm_mqr <- MQR_gbm(data = h2,
-                  formula = n_attendance ~ clock_hour + doy,
+                  formula = n_attendance ~ clock_hour + dow + doy + n_att_rollmean_lag48,
                   quantiles = seq(0.05,0.95,by=0.05),
                   gbm_params = list(n.tree=300,
                                     interaction.depth=2,
